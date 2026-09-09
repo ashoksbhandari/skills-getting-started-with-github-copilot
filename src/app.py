f0d66fbbc84c233714bbae7a8d7b589b7af1331d
ashoksbhandari <37208951+ashoksbhandari@users.noreply.github.com>
2026-09-09 05:38:36 +0000

    Add 2 more sports related activities, 2 more artistic activities, and 2 more intellectual activities.

--- src/app.py
+++ src/app.py
@@ -62,7 +62,7 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specific activity
     activity = activities[activity_name]
 
-    # Check if student is already signed up
+    # Check if student is already signed up -- Validate
     if email in activity["participants"]:
         raise HTTPException(
             status_code=400,
